40fca0d672a57c572ed4acf468c17e6fe0ac69b0
04AJ <alanjcd2004@gmail.com>
2024-02-10 14:21:59 -0600

    added trips page

---
+++ app/(pages)/trips/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
+import { useEffect, useState } from "react";
+
+export default function Trips() {
+  const [notes, setNotes] = useState<any[] | null>(null);
+  const supabase = createClient();
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await supabase.from("USER").select();
+      setNotes(data);
+    };
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Test</h1>
+      <pre>{JSON.stringify(notes, null, 2)}</pre>
+    </div>
+  );
+}
